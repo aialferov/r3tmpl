3e3ff65822ea830502a3c625217fdb6e2f8e1d94
Anton Alferov <anton@alferov.me>
2018-09-28 12:10:57 +0200

    Update Mk
    Use *.app.src.script only file
    Add Common Test related targets

--- templates/mk/Erlanglib.mk
+++ templates/mk/Erlanglib.mk
@@ -1,5 +1,5 @@
-PROJECT = $(shell grep {application, src/*.app.src | cut -d" " -f2 | tr -d "',")
-VERSION = $(shell grep {vsn, src/$(PROJECT).app.src | cut -d\" -f2)
+PROJECT = $(shell grep {application, src/*.script | cut -d" " -f2 | tr -d "',")
+VERSION = $(shell grep {vsn, src/$(PROJECT).app.src.script | cut -d\" -f2)
 GIT_SHA = $(shell git rev-parse HEAD | cut -c1-8)
 
 REBAR = $(shell which ./rebar3 || which rebar3)
@@ -8,6 +8,18 @@ BASE_PATH = _build/default
 
 LIB_PATH = $(BASE_PATH)/lib
 PLUGIN_PATH = $(BASE_PATH)/plugins
+
+CT_SUITES_PATH = $(LIB_PATH)/$(PROJECT)/ebin
+CT_LOG_PATH = $(BASE_PATH)/ct_logs
+
+UNAME = $(shell uname)
+
+ifeq ($(UNAME), Darwin)
+    CT_OPEN_CMD = open
+endif
+ifeq ($(UNAME), Linux)
+    CT_OPEN_CMD = xdg-open
+endif
 
 USAGE_PADDING = 14
 
@@ -43,6 +55,14 @@ upgrade:
 	$(REBAR) upgrade
 	$(REBAR) unlock
 
+ct:
+	$(REBAR) ct \
+		--dir $(CT_SUITES_PATH) \
+		--logdir $(CT_LOG_PATH)
+
+ct-open:
+	$(CT_OPEN_CMD) $(CT_LOG_PATH)/index.html
+
 git-release:
 	git tag -a $(VERSION)
 	git push origin $(VERSION)
@@ -55,7 +75,7 @@ usage:
 	$(usage)
 
 define usage-erlanglib-targets
-	@printf '$(shell printf "    %%-$(USAGE_PADDING)s %%s\\\n%.0s" {1..9})' \
+	@printf '$(shell printf "    %%-$(USAGE_PADDING)s %%s\\\n%.0s" {1..11})' \
 	compile \
 		"Compile including downloading and compiling dependencies (default)" \
 	check "Run EUnit based unit tests" \
@@ -64,6 +84,8 @@ define usage-erlanglib-targets
 	shell "Run Erlang shell with all the modules compiled and loaded" \
 	deps "Download dependencies" \
 	upgrade "Upgrade dependencies" \
+	ct "Run Erlang Common Tests" \
+	ct-open "Open browser with Common Tests results" \
 	git-release "Create and push a git tag named after current version" \
 	version "Print current version and git SHA"
 endef
